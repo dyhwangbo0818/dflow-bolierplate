cbb88fbfe07659fd527817134cbeedf5dee88a29
dyhwangbo <dukyoon.hwangbo@nhnad.com>
2025-03-10 14:24:27 +0900

    server 주석 처리

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig, loadEnv} from "vite";
 
 export default ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
-    const API_URL = `${env.VITE_APP_HOST}:${env.VITE_APP_PORT}`;
+    //const API_URL = `${env.VITE_APP_HOST}:${env.VITE_APP_PORT}`;
     //console.log(`VITE CONFIG API URL : ${API_URL}`);
 
     return defineConfig({
@@ -22,16 +22,6 @@ export default ({ mode }) => {
         server: {
             port : 3030,
             open : true,
-            proxy: {
-                '/auth': {
-                    target: API_URL,
-                    changeOrigin: true,
-                    // SSL 인증서 검증 무시
-                    //secure: false,
-                    // WebSocket 프로토콜 사용
-                    //ws: true,
-                }
-            }
         },
         preview: {
             port: 3030
